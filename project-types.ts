b6c1d3b6acecb3c2a9bb84121bcc8eb23d132629
AFKJonas <jonasengelsgaard@gmail.com>
2026-09-08 14:54:45 +0200

    lagt til oppgaveteksten

---
+++ project-types.ts
@@ -0,0 +1,5 @@
+/*- Hver person i gruppa lager minst en type og funksjon med signatur relevant for prosjektet dere skal lage
+- Diskutere dette i fellesskap (retrospekt kan få spørsmål om dette)
+- Lage en prioiteringsliste på hva prosjektet skal ta for seg
+- Og en kuttliste hvis noe må ut*/
+
