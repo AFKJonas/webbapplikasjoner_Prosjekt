56b2e33d2070de45f690bc87b1f3a1bb3397ea6d
AFKJonas <jonasengelsgaard@gmail.com>
2026-09-18 10:25:43 +0200

    skrev litt tekst

--- project-types.ts
+++ project-types.ts
@@ -1,5 +1,21 @@
 /*- Hver person i gruppa lager minst en type og funksjon med signatur relevant for prosjektet dere skal lage
 - Diskutere dette i fellesskap (retrospekt kan få spørsmål om dette)
 - Lage en prioiteringsliste på hva prosjektet skal ta for seg
-- Og en kuttliste hvis noe må ut*/
+- Og en kuttliste hvis noe må ut
+
+
+- Lage egent API
+- Lage en prototype på hvordan APIet skal se ut
+-  
+
+
+
+
+
+
+
+
+
+
+*/
 
